5d5ac1f3061c5d2aa52af4b41e3b717b179a0a2f
joancrookston-c <joanemily@live.com>
2020-02-07 08:59:22 -0500

    No changes, just can't pull

--- 19_Capstone/Capstone/Classes/VendingMachine.cs
+++ 19_Capstone/Capstone/Classes/VendingMachine.cs
@@ -110,13 +110,13 @@ namespace Capstone.Classes
             string timeStamp = string.Format(("{0:yyyy-MM-dd_hh-mm-ss}"), DateTime.Now);
             string path = ($"C:\\Users\\Student\\git\\c-module-1-capstone-team-5\\19_Capstone\\SalesReports\\{timeStamp}.txt");
 
-            File.Create(path);
+            
 
                 using (StreamWriter report = new StreamWriter(path))
                 {
                     foreach (KeyValuePair<string, Item> item in items)
                     {
-                    report.WriteLine($"{item} | {item.Value.Quantity}");
+                    report.WriteLine($"{item.Value} | {item.Value.Quantity}");
                     }
 
                 report.WriteLine($"Total Sales: ${GetTotalSales}");
